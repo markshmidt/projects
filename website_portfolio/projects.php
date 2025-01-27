ecdb76c102c1223489764ca5a481ea289ceb6e87
Mariia Shmidt <masha.shmidt.04@gmail.com>
2025-01-27 23:33:26 +0300

    last version of tic-tac-toe + details of portfolio

--- website_portfolio/projects.php
+++ website_portfolio/projects.php
@@ -5,22 +5,22 @@
         <!-- Project 1 -->
         <div class="col-md-4">
             <div class="card h-100">
-                <img src="assets/images/project1.jpg" class="card-img-top" alt="Project 1 Screenshot">
+                <img src="assets/img/project-maven.gif" class="card-img-top" alt="Racing game">
                 <div class="card-body">
-                    <h5 class="card-title">Project 1</h5>
-                    <p class="card-text">A brief description of Project 1. This project demonstrates my skills in [technology/feature].</p>
-                    <a href="https://github.com/yourusername/project1" class="btn btn-primary" target="_blank">View on GitHub</a>
+                    <h5 class="card-title">Racing game</h5>
+                    <p class="card-text">JavaFX game using the JavaRush graphics engine. Practicing writing dependencies and plugins in pom.xml file.</p>
+                    <a href="https://github.com/markshmidt/projects/tree/main/JavaProjects/project-maven" class="btn btn-primary" target="_blank">View on GitHub</a>
                 </div>
             </div>
         </div>
         <!-- Project 2 -->
         <div class="col-md-4">
             <div class="card h-100">
-                <img src="assets/images/project2.gif" class="card-img-top" alt="Project 2 Screenshot">
+                <img src="assets/img/tic-tac-toe.gif" class="card-img-top" alt="Tic Tac Toe gif">
                 <div class="card-body">
-                    <h5 class="card-title">Project 2</h5>
-                    <p class="card-text">A brief description of Project 2. It focuses on [technology/feature].</p>
-                    <a href="https://github.com/yourusername/project2" class="btn btn-primary" target="_blank">View on GitHub</a>
+                    <h5 class="card-title">Tic-tac-toe game</h5>
+                    <p class="card-text">Tic-Tac-Toe game on Tomcat with focus on using servlets and JSP as well as working with HTTP requests ans responses.</p>
+                    <a href="https://github.com/markshmidt/projects/tree/main/JavaProjects/project-servlet" class="btn btn-primary" target="_blank">View on GitHub</a>
                 </div>
             </div>
         </div>
