259c168f651e94b84e4a911808f0e2884da90395
Mariia Shmidt <masha.shmidt.04@gmail.com>
2025-02-20 06:05:41 +0300

    new project structure

---
+++ JavaProjects/quest/src/main/java/org/example/quest/GameServlet.java
@@ -0,0 +1,9 @@
+package org.example.quest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class GameServlet extends Application {
+
+}
---
+++ JavaProjects/quest/src/main/java/org/example/quest/StartServlet.java
@@ -0,0 +1,14 @@
+package org.example.quest;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class StartServlet {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
